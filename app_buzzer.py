93fc1745414e9f7178f3c83350bb71269a0d7136
Corentin Lapeyre <corentin.lapeyre@gmail.com>
2021-12-06 17:31:33 +0100

    Total overhaul of buzzer app (4 colors)

--- app_buzzer.py
+++ app_buzzer.py
@@ -1,47 +1,68 @@
-from lib.buzzer import buzzer
-from lib.buttons import button_init, on, off, _all_buttons
-import uasyncio
-import utime
+import uasyncio as asyncio
+import urandom as random
+from lib.buzzer import get_buzzer
+from lib.buttons import get_arcadebuttons, get_controlpanel
+from lib.sh1107 import get_oled
 
 
-async def blink(led, up, down, times):
-    for _ in range(times):
-        on(led)
-        await uasyncio.sleep_ms(up)
-        off(led)
-        await uasyncio.sleep_ms(down)
-
-
-async def buzz(tune):
-    tunes = [[220, 220*1.414, 440, 440*1.414, 880],
-             [220, 220*1.414, 440, 440*1.414, 880][::-1],
-             [220, 220*1.414, 220, 440, 220, 440*1.414],
-             [220, 220*1.414, 220, 440, 220, 440*1.414][::-1],
-             ]
-    for tone in tunes[tune]:
-        buzzer.tone(int(tone), 60, 0.1)
-        await uasyncio.sleep_ms(30)
+async def buzz(color):
+    tunes = {"white": [220, 220*1.414, 440, 440*1.414, 880],
+             "blue": [220, 220*1.414, 440, 440*1.414, 880][::-1],
+             "red": [220, 220*1.414, 220, 440, 220, 440*1.414],
+             "yellow": [220, 220*1.414, 220, 440, 220, 440*1.414][::-1],
+             }
+    buzzer = get_buzzer()
+    for tone in tunes[color]:
+        await asyncio.create_task(buzzer.tone(int(tone), 60, 0.1))
 
 
 async def app_buzzer():
-    print('Bienvenue dans le buzzer!')
-    buttons, leds = button_init()
-    buzzers = [pair for i, pair in enumerate(zip(buttons, leds))
-               #if i in (0, 1)
-               ]
+    print('  >>>  Bienvenue dans le buzzer!  <<<')
 
-    map(on, leds)
-    print("Ready to buzz?")
-    while True:
-        list(map(on, leds))
-        for idx, (button, led) in enumerate(buzzers):
-            if not button.value():
-                list(map(off, leds))
-                uasyncio.create_task(buzz(idx))
-                await uasyncio.create_task(blink(led, 100, 100, 3))
-                print("Ready to buzz?")
-        #if not _all_buttons()[0][-1].value():
-        #    return
+    arcade = get_arcadebuttons(pressed_flag=True)
+    arcade.reset_flags()
+    arcade.on()
+    cp = get_controlpanel(pressed_flag=True)
+    cp.reset_flags()
+
+    oled = get_oled()
+    print("  >>>  Ready to buzz?  <<<")
+    oled.fill(0)
+    oled.text("Buzz!", 0, 0, 1)
+    oled.show()
+
+    while 'select' not in cp.pressed:
+        while not arcade.pressed:
+            await asyncio.sleep_ms(1)
+
+        if arcade.pressed:
+            arcade.off()
+            choice = 0
+            if len(arcade.pressed) > 1:
+                # This shouldn't happen...
+                oled.text('E ' + str(arcade.pressed), 0, 120, 1)
+                oled.show()
+                choice = random.randint(0, len(arcade.pressed)-1)
+            idx = arcade.pressed[choice]
+            await asyncio.gather(
+                buzz(arcade.color[idx]),
+                arcade.leds[idx].blink(100, 100, 3))
+            arcade.leds[idx].on()
+            await asyncio.sleep_ms(200)
+            arcade.reset_flags()
+        
+            while not arcade.pressed:
+                if 'select' in cp.pressed:
+                    return
+                await asyncio.sleep_ms(20)
+            await asyncio.sleep_ms(500)
+            for _ in range(2):
+                arcade.off()
+                await asyncio.sleep_ms(100)
+                arcade.on()
+                await asyncio.sleep_ms(100)
+            arcade.reset_flags()
+    arcade.off()
 
 if __name__ == '__main__':
-    uasyncio.run(app_buzzer())
+    asyncio.run(app_buzzer())
