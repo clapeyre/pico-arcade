2946cbf4c162276753c8ecd3bcd5a9c889843d7d
Corentin Lapeyre <corentin.lapeyre@gmail.com>
2021-12-06 17:25:26 +0100

    Switch buzzer to async, singleton approach

--- lib/buzzer.py
+++ lib/buzzer.py
@@ -1,11 +1,12 @@
 from machine import Pin, PWM
-import time
+import utime as time
+import uasyncio as asyncio
 
-class Buzzer(PWM):
-    def tone(self, frequency, duration, level=0.2):
+class _Buzzer(PWM):
+    async def tone(self, frequency, duration, level=0.2):
         self.freq(frequency)
         self.duty_u16(int(0.95 * level * 65535))
-        time.sleep_ms(duration)
+        await asyncio.sleep_ms(duration)
         self.duty_u16(0)
 
     def start_tone(self, frequency, level=0.2):
@@ -15,7 +16,16 @@ class Buzzer(PWM):
     def end_tone(self):
         self.duty_u16(0)
 
-buzzer = Buzzer(Pin(14))
+
+_BUZZER = None
+
+def get_buzzer():
+    global _BUZZER
+    if _BUZZER is None:
+        _BUZZER = _Buzzer(Pin(14))
+    return _BUZZER
+
+buzzer = get_buzzer()
 
 if __name__ == '__main__':
     buzzer.tone(440, 250, 1)
