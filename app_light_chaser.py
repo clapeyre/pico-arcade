0055a08dce8248a9834604379e8581fe24c64df0
Corentin Lapeyre <corentin.lapeyre@gmail.com>
2021-12-06 17:30:30 +0100

    Overhaul of light-chaser
    Async, much cleaner approach.
    Tests do now work properly, I have an asyncio problem...

--- app_light_chaser.py
+++ app_light_chaser.py
@@ -1,29 +1,12 @@
-import utime
-import _thread
-import urandom
+import utime as time
+import urandom as random
 import uasyncio as asyncio
-from lib.buttons import button_init, on, off
+from lib.buttons import get_arcadebuttons
+from lib.buzzer import get_buzzer
 from lib.sh1107 import get_oled
 
 
-#score = 0
-async def scorer():
-    global score
-    # from lcd1602 import LCD
-    # lcd = LCD()
-    new_score = -1
-    while True:
-        if new_score != score:
-            print(f"Light-chaser 0.1\n    Score: {score}")
-            # lcd.clear()
-            # lcd.message(f"Light-chaser 0.1\n    Score: {score}")
-            # lcd.openlight()
-            new_score = score
-        await asyncio.sleep_ms(100)
-#_thread.start_new_thread(scorer, ())
-
-# timer_click = 0
-def app_light_chaser(hardcore=False, timer=10_000):
+async def app_light_chaser(hardcore=False, timer=20_000, nleds=3):
     oled = get_oled()
     print('Bienvenue dans le chasseur de lumière!')
     oled.fill(0)
@@ -34,56 +17,71 @@ def app_light_chaser(hardcore=False, timer=10_000):
         oled.text("Hardcore", 0, 20, 1)
     oled.show()
     score = 0
-    start = utime.ticks_ms()
-    buttons, leds = button_init()
-    recent = -1
-    #on(leds[urandom.randint(0, len(leds) - 1)])
-    while True:
-        # print("Setting a random LED to 1")
-        while True:
-            rnd = urandom.randint(0, len(leds) - 1)
-            if not leds[rnd].value() and buttons[rnd].value() and rnd != recent:
-                on(leds[rnd])
-                recent = rnd
-                break
-            utime.sleep_ms(1)
+    start = time.ticks_ms()
+    arcade = get_arcadebuttons(pressed_flag=True)
+    arcade.reset_flags()
+    arcade.off()
 
-        light_found = False
-        while not light_found:
-            for button, led in zip(buttons, leds):
-                if not button.value():
-                    # print(f'led status: {[led.value() for led in leds]}')
-                    # print(f'button status: {[but.value() for but in buttons]}')
-                    # utime.sleep(0.3)
-                    if led.value():
-                        off(led)
-                        light_found = True
-                        score += 1
-                        while not button.value():
-                            utime.sleep_ms(1)
-                        #print(f"Score is: {score}")
-                        #print(utime.ticks_diff(start, utime.ticks_ms()))
-                    elif hardcore:
-                        while not button.value():
-                            utime.sleep_ms(1)
-                        score -= 1
-            utime.sleep_ms(1)
-        if utime.ticks_diff(utime.ticks_ms(), start) >= timer:
-            print('\n >>> FINI <<<')
-            print(f"\n Score: {score}")
-            oled.fill(0)
-            oled.text('Fini!', 0, 0, 1)
-            oled.text('Score:', 0, 20, 1)
-            oled.text(f'  {score}', 0, 40, 1)
-            oled.show()
-            return
-        # else:
-        #     print(f"                   Debug: {score}")
+    def light_random(forbidden=None):
+        """Light a random LED that is off (not in forbidden)"""
+        if not forbidden:
+            forbidden = []
+        while True:
+            rnd = random.randint(0, arcade.size - 1)
+            if not arcade.leds[rnd].value() and rnd not in forbidden:
+                break
+            # time.sleep_ms(1)
+        arcade.leds[rnd].on()
+        return rnd
+
+    for _ in range(nleds):
+        light_random()
+
+    while True:
+        while not arcade.pressed and time.ticks_diff(time.ticks_ms(), start) < timer:
+            await asyncio.sleep_ms(1)
+
+        if time.ticks_diff(time.ticks_ms(), start) >= timer:
+            break
+
+        found = [i for i in arcade.pressed if arcade.leds[i]()]
+        for _ in found:
+            light_random()
+        for idx in found:
+            arcade.leds[idx].off()
+        score += len(found)
+
+        if hardcore:
+            score -= (len(arcade.pressed) - len(found))
+        
+        arcade.reset_flags()
+
+    arcade.off()
+    print('\n >>> FINI <<<')
+    print(f"\n Score: {score}")
+    oled.fill(0)
+    oled.text('Fini!', 0, 0, 1)
+    oled.text('Score:', 0, 20, 1)
+    oled.text(f'  {score}', 0, 40, 1)
+    oled.show()
+
+    buzzer = get_buzzer()
+    for tone in [880, 440*1.414, 440, 220*1.414, 220]:
+        await asyncio.create_task(buzzer.tone(int(tone), 60, 0.5))
+        time.sleep_ms(30)
+
+    return
+
+async def test():
+    while True:
+        asyncio.run(app_light_chaser(timer=1000))
+        time.sleep(1)
+        print('monitor restart')
+        arcade = get_arcadebuttons(pressed_flag=True)
+        arcade.reset_flags()
+        while not arcade.pressed:
+            await asyncio.sleep_ms(1)
+        # print(arcade.pressed)
 
 if __name__ == '__main__':
-    buttons, leds = button_init()
-    while True:
-        app_light_chaser()
-        utime.sleep(0.3)
-        while buttons[0].value():
-            utime.sleep(0.01)
+    asyncio.run(test())
