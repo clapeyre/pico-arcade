edaa0b49e2bc413d3ac5c9bb6c3795fc32997696
Corentin Lapeyre <corentin.lapeyre@gmail.com>
2022-01-02 10:58:57 +0100

    Update app_light for libs, digitalscorer

--- app_light_chaser.py
+++ app_light_chaser.py
@@ -1,13 +1,14 @@
 import utime as time
 import urandom as random
 import uasyncio as asyncio
+from drivers.buzzer import Buzzer
+from drivers.sh1107 import SH1107_I2C
 from lib.buttons import get_arcadebuttons
-from lib.buzzer import get_buzzer
-from lib.sh1107 import get_oled
+from lib.score import DigitalScorer
 
 
 async def app_light_chaser(hardcore=False, timer=20_000, nleds=3):
-    oled = get_oled()
+    oled = SH1107_I2C()
     print('Bienvenue dans le chasseur de lumière!')
     oled.fill(0)
     oled.text("Chasse", 0, 0, 1)
@@ -24,7 +25,7 @@ async def app_light_chaser(hardcore=False, timer=20_000, nleds=3):
 
     def light_random(forbidden=None):
         """Light a random LED that is off (not in forbidden)"""
-        if not forbidden:
+        if forbidden is None:
             forbidden = []
         while True:
             rnd = random.randint(0, arcade.size - 1)
@@ -37,9 +38,14 @@ async def app_light_chaser(hardcore=False, timer=20_000, nleds=3):
     for _ in range(nleds):
         light_random()
 
+    printed = 0
+    print()
     while True:
         while not arcade.pressed and time.ticks_diff(time.ticks_ms(), start) < timer:
             await asyncio.sleep_ms(1)
+            if time.ticks_diff(time.ticks_ms(), start) > (printed + 1) * 1000:
+                printed += 1
+                print(printed, end=' ')
 
         if time.ticks_diff(time.ticks_ms(), start) >= timer:
             break
@@ -65,12 +71,18 @@ async def app_light_chaser(hardcore=False, timer=20_000, nleds=3):
     oled.text(f'  {score}', 0, 40, 1)
     oled.show()
 
-    buzzer = get_buzzer()
+    buzzer = Buzzer()
     for tone in [880, 440*1.414, 440, 220*1.414, 220]:
         await asyncio.create_task(buzzer.tone(int(tone), 60, 0.5))
         time.sleep_ms(30)
 
+    d = DigitalScorer()
+    await d.interruptable_score(score)
+
     return
+
+def menu_light_chaser():
+    asyncio.run(app_light_chaser())
 
 async def test():
     while True:
